f6b534f1c81bf5fcf68213c4f402dce8f75c92d4
Lasha <93600887+Vanishvili@users.noreply.github.com>
2022-06-09 17:22:58 +0400

    Update Main.tf

--- 1-RDS/Main.tf
+++ 1-RDS/Main.tf
@@ -1,1 +1,30 @@
+module "db_default" {
+  source = "terraform-aws-modules/rds/aws"
 
+  identifier = "${local.name}-default"
+
+  create_db_option_group    = false
+  create_db_parameter_group = false
+
+  engine               = "mysql"
+  engine_version       = "8.0.27"
+  family               = "mysql8.0"
+  major_engine_version = "8.0"
+  instance_class       = "db.t4g.medium"
+
+  allocated_storage = 20
+
+  db_name  = "completeMysql"
+  username = "complete_mysql"
+  port     = 3306
+
+  subnet_ids             = module.vpc.database_subnets
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  backup_retention_period = 0
+
+  tags = local.tags
+}
